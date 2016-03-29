8fe3f1092e0582e4aabc282d003ae31297708027
Kotokrad <dsl2407@gmail.com>
2016-03-29 15:27:56 +0300

    ssh mode

---
+++ git-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+user="kotokrad"
+repo="${PWD##*/}" #current folder name
+https=false
+
+if [[ "$https" = true ]]; then
+	url="https://github.com/$user/$repo.git"
+else
+	url="git@github.com:$user/$repo.git"
+fi
+
+git init
+git add .
+git commit -m "initial commit"
+git remote add origin "$url"
+git push -u origin master
+git subtree push --prefix dist origin gh-pages
