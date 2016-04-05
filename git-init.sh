e686d37714def7f64bfce16db117f660b88fba87
Kotokrad <dsl2407@gmail.com>
2016-04-05 22:31:34 +0300

    repo creating & default username

--- git-init.sh
+++ git-init.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-user="kotokrad"
+user="%default%"
 repo="${PWD##*/}" #current folder name
 https=false
+gitignore=(
+	"node_modules"
+	"bower_components"
+	)
+
+if [[ "$user" = "%default%" ]] || [[ "$1" = "-changeuser" ]]; then
+	read -p "GitHub username: " -e newuser
+	if [ -n "$newuser" ]; then
+		sed -i -e "0,/$user/ s/$user/$newuser/" "${0##*/}"
+		user="$newuser"
+	else
+		echo "Not now? Okay.."
+		exit
+	fi
+fi
+
+printf "%s\n" "${gitignore[@]}" > .gitignore
 
 if [[ "$https" = true ]]; then
 	url="https://github.com/$user/$repo.git"
@@ -9,6 +26,7 @@ else
 	url="git@github.com:$user/$repo.git"
 fi
 
+curl -sS -u "$user" -d '{"name": "$repo"}' https://api.github.com/user/repos > /dev/null
 git init
 printf "\ngit-deploy.sh\ngit-init.sh" >> .gitignore
 git add .
